13365f05bd52fbae58152dc5a11c73a1f19dcf89
Marcin Starzynski <gienekmsms@gmail.com>
2021-05-16 14:32:35 +0200

    Add Heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -6,11 +8,19 @@ const testRoutes = require('./routes/test.routes');
 const concertRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/', testRoutes);
-app.use('/', concertRoutes);
-app.use('/', seatsRoutes);
+app.use('/api', testRoutes);
+app.use('/api', concertRoutes);
+app.use('/api', seatsRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+  
 
 app.use((req, res) => {
     return res.json({
@@ -18,6 +28,6 @@ app.use((req, res) => {
     });
 });
 
-app.listen(7000, () => {
+app.listen(process.env.PORT || 7000, () => {
     console.log('Server running on port 7000');
 });
